7c10bf101668248184985ad777bcfafd245cfaa6
Bernhard Grünewaldt <bernhard.gruenewaldt@comsysto.com>
2019-12-11 12:32:53 +0100

    removing headless

--- e2e/tests/smoke.test.ts
+++ e2e/tests/smoke.test.ts
@@ -7,10 +7,11 @@ const baseUrl = <string>process.env.BASE_URL
 
 console.log('BASEURL: ', baseUrl)
 
-fixture('Smoke-Test').page(`${baseUrl}/`)
-//  .beforeEach(async (t: TestController) => {
-//    await t.maximizeWindow()
-//  })
+fixture('Smoke-Test')
+  .page(`${baseUrl}/`)
+  .beforeEach(async (t: TestController) => {
+    await t.maximizeWindow()
+  })
 
 test('Page should display hello', async (t: TestController) => {
   await t.expect(Selector('div').withText('Hello').exists).ok('hello should be shown')

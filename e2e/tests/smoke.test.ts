83b09da8ebd817cf55e8604be69f03295ea48c38
Bernhard Grünewaldt <bernhard.gruenewaldt@comsysto.com>
2019-12-11 11:45:37 +0100

    fix

--- e2e/tests/smoke.test.ts
+++ e2e/tests/smoke.test.ts
@@ -2,11 +2,10 @@ import { Selector } from 'testcafe'
 
 const baseUrl = <string>process.env.BASE_URL
 
-fixture('Smoke-Test')
-  .page(`${baseUrl}/`)
-  .beforeEach(async (t: TestController) => {
-    await t.maximizeWindow()
-  })
+fixture('Smoke-Test').page(`${baseUrl}/`)
+//  .beforeEach(async (t: TestController) => {
+//    await t.maximizeWindow()
+//  })
 
 test('Page should display hello', async (t: TestController) => {
   await t.expect(Selector('div').withText('Hello').exists).ok('hello should be shown')
